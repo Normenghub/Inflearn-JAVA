65d55951168736899adec266422c99948afc8a1e
SHEEN <normengdie@pusan.ac.kr>
2024-08-16 16:20:19 +0900

    feat : add fun

--- Method/Method.java
+++ Method/Method.java
@@ -1,4 +1,18 @@
 package Method;
 
+import java.util.Scanner;
+
 public class Method {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+        System.out.println(add(a,b));
+    }
+    // add 메서드
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
 }
