cd950556f77d0a8cab8c2453034a42883ac03d1f
SHEEN <normengdie@pusan.ac.kr>
2024-08-16 16:35:30 +0900

    docs : create Method2.java

---
+++ Method/Method2.java
@@ -0,0 +1,14 @@
+package Method;
+
+public class Method2 {
+    // No return
+    public static void main(String[] args) {
+        print();
+    }
+    public static void print() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
