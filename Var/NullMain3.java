882f7b7c81c463ca4fe84b23542ea6573f1d43a9
SHEEN <normengdie@pusan.ac.kr>
2024-08-21 21:18:29 +0900

    fix : NullMain3.java

--- Var/NullMain3.java
+++ Var/NullMain3.java
@@ -3,6 +3,7 @@ package Var;
 public class NullMain3 {
     public static void main(String[] args) {
         BigData bigData = new BigData();
+        bigData.data = new Data();
         System.out.println("bigData.data" + bigData.data);
         System.out.println("bigData.count" + bigData.count);
 
