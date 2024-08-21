759e2c7d31208ace461a6a8661d912b92200e8c4
SHEEN <normengdie@pusan.ac.kr>
2024-08-21 21:35:38 +0900

    docs : create OOP1.java

--- oop/OOP1.java
+++ oop/OOP1.java
@@ -1,5 +1,29 @@
 package oop;
 
 public class OOP1 {
+    public static void main(String[] args) {
+        int volume = 0;
+        boolean isOn = false;
+
+        isOn = true;
+        System.out.println("음악 플레이어를 시작합니다.");
+
+        volume++;
+        System.out.println("음악 플레이어 볼륨: " + volume);
+
+        volume++;
+        System.out.println("음악 플레이어 볼륨: " + volume);
+
+        volume--;
+        System.out.println("음악 플레이어 볼륨: " + volume);
+
+        System.out.println("음악 플레이어 상태 확인");
+        if(isOn){
+            System.out.println("음악 플레이어 ON, 볼륨: " +volume);
+        }else{
+            System.out.println("음악 플레이어 OFF, 볼륨: " +volume);
+        }
+    }
+
 
 }
