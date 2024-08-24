980360517fe02a262b37bd81d3fce80f013f30ff
SHEEN <normengdie@pusan.ac.kr>
2024-08-24 13:53:13 +0900

    docs : create ValueObjectMain.java

---
+++ oop/ValueObjectMain.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class ValueObjectMain {
+    public static void main(String[] args) {
+        ValueData data = new ValueData();
+        data.add();
+        data.add();
+        data.add();
+    }
+}
