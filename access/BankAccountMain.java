dd1f4019ba4ca27680d7ed072c6f5bcfa82a6223
SHEEN <normengdie@pusan.ac.kr>
2024-08-25 16:56:36 +0900

    docs : create BankAcoountMain.java

---
+++ access/BankAccountMain.java
@@ -0,0 +1,13 @@
+package access;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+
+        account.deposit(10000);
+        account.withdraw(5000);
+
+        System.out.println("잔액 = " + account.getBalance());
+    }
+
+}
