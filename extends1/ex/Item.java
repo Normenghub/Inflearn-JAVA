8b7eee47bd238c0ead71738bfc617e4d6109f175
SHEEN <normengdie@pusan.ac.kr>
2024-08-31 13:20:24 +0900

    docs : create Item.java

---
+++ extends1/ex/Item.java
@@ -0,0 +1,18 @@
+package extends1.ex;
+
+public class Item {
+     String name;
+     int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    int getPrice(){
+        return this.price;
+    }
+    void print(){
+        System.out.println("이름 : " + name + ", 가격 : " + price);
+    }
+
+}
